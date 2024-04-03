9433b42803268c8fa1301a299a56462394c3a2f9
david-barrei <davbarreiro4@gmail.com>
2024-04-02 23:58:11 -0500

    documentando

--- Tarea/aplications/users/views.py
+++ Tarea/aplications/users/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 #Crear los login con validaciones por correo electronico
+# Enviar mensajes por correo electronico al usuario
+#hacer un chat en la aplicacion
