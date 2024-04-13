ceb22b29a6c0d4e9d9fec38b745ab3623c858e67
david-barrei <davbarreiro4@gmail.com>
2024-04-12 20:13:13 -0500

    creando login de usuarios

--- Tarea/aplications/users/views.py
+++ Tarea/aplications/users/views.py
@@ -6,7 +6,5 @@ from django.shortcuts import render
 # Enviar mensajes por correo electronico al usuario
 #hacer un chat en la aplicacion
 
-class UserRegisterView(FormView):
-    template_name = 'users/register.html'
-    form_class = UserRegisterForm
-    success_url = '/'
+
+
